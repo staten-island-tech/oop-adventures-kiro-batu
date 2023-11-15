42dd869e07b10d2de9d2681b378b2c573f6b0b1a
BatuhanCaglar <fbmanbc@gmail.com>
2023-11-15 10:59:29 -0500

    um m

---
+++ armor.py
@@ -0,0 +1,6 @@
+class Armor():
+    def demstats (self, type, health, speed):
+        self.type = type 
+        self.health = health
+        self.speed = speed 
+    demarmors = {'type': type, 'health': health , 'speed': speed,}
