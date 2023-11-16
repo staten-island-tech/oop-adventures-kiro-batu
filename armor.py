94834e2ff031d079aa511cd7dbf15c9069ecc295
BatuhanCaglar <fbmanbc@gmail.com>
2023-11-16 14:00:24 -0500

    aggag

--- armor.py
+++ armor.py
@@ -1,6 +1,31 @@
-class Armor():
-    def demstats (self, type, health, speed):
-        self.type = type 
-        self.health = health
-        self.speed = speed 
-    demarmors = {'type': type, 'health': health , 'speed': speed,}
+import random 
+
+class Shield():
+    itemtype = ("Armor")
+    def health():
+        start = 5
+        end = 15
+        values = list(range(start, end)) 
+        randomnum = (random.choice(values))
+        print ("This shield provides a boost of",randomnum, "to your health")
+    def speed ():
+        start = 5
+        end = 15
+        values = list(range(start, end))
+        randomnum = (random.choice(values))
+        print ("Yet this shield also makes you slower by", randomnum )
+    health()
+    speed()
+Shield()
+
+class Boots():
+    itemtype = ("Boots")
+    def speed():
+        start = 5
+        end = 15
+        values = list(range(start, end)) 
+        randomnum = (random.choice(values))
+        print ("This pair of boots provides a boost of",randomnum, "to your health")
+    speed()
+Boots(): 
+
