a73a9383b5fe68b609903b3fe8d483995eeaf210
BatuhanCaglar <fbmanbc@gmail.com>
2023-11-17 13:37:05 -0500

    yippee progress

--- armor.py
+++ armor.py
@@ -1,31 +1,70 @@
 import random 
 
-class Shield():
-    itemtype = ("Armor")
-    def health():
-        start = 5
-        end = 15
-        values = list(range(start, end)) 
-        randomnum = (random.choice(values))
-        print ("This shield provides a boost of",randomnum, "to your health")
-    def speed ():
-        start = 5
-        end = 15
-        values = list(range(start, end))
-        randomnum = (random.choice(values))
-        print ("Yet this shield also makes you slower by", randomnum )
-    health()
-    speed()
-Shield()
 
-class Boots():
-    itemtype = ("Boots")
-    def speed():
-        start = 5
-        end = 15
-        values = list(range(start, end)) 
-        randomnum = (random.choice(values))
-        print ("This pair of boots provides a boost of",randomnum, "to your health")
-    speed()
-Boots(): 
+class item():
+    class Shield():
+        itemtype = ("Armor")
+        def health():
+            start = 5
+            end = 15
+            values = list(range(start, end)) 
+            randomnum = (random.choice(values))
+            print ("This shield provides a boost of",randomnum, "to your health")
+        def speed ():
+            start = 5
+            end = 15
+            values = list(range(start, end))
+            randomnum = (random.choice(values))
+            print ("Yet this shield also makes you slower by", randomnum )
+        health()
+        speed()
+    Shield()
+
+    class Boots():
+        itemtype = ("Armor")
+        def speed():
+            start = 5
+            end = 15
+            values = list(range(start, end)) 
+            randomnum = (random.choice(values))
+            print ("This pair of boots provides a boost of",randomnum, "to your speed")
+        speed()
+    Boots()
+
+    class Chestplate():
+        itemtype = ("Armor")
+        def health():
+            start = 10
+            end = 20
+            values = list(range(start, end)) 
+            randomnum = (random.choice(values))
+            print ("This chestplate provides a boost of",randomnum, "to your health")
+        def speed ():
+            start = 5
+            end = 15
+            values = list(range(start, end))
+            randomnum = (random.choice(values))
+            print ("Yet this chestplate also makes you slower by", randomnum )
+        health()
+        speed()
+    Chestplate()
+
+    class Cloak():
+        itemtype = ("Armor")
+        def health():
+            start = 5
+            end = 10
+            values = list(range(start, end)) 
+            randomnum = (random.choice(values))
+            print ("This cloack provides a boost of",randomnum, "to your health")
+        def speed ():
+            start = 10
+            end = 20
+            values = list(range(start, end))
+            randomnum = (random.choice(values))
+            print ("This cloak also makes you faster by", randomnum )
+        health()
+        speed()
+    Cloak()
+item()
 
