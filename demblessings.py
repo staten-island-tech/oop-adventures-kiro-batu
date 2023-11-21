233f9b2cbb86beb771af34ee79ff97d5b81d318b
BatuhanCaglar <fbmanbc@gmail.com>
2023-11-21 13:57:59 -0500

    aa

--- demblessings.py
+++ demblessings.py
@@ -1,6 +1,5 @@
 import random
-import json
-import os
+
 
 class Blessings():
     class Hercules():
@@ -11,9 +10,7 @@ class Blessings():
             randomnum = (random.choice(values))
             Herculesbuff = randomnum
             print ("This charm is imbued with the vitality of Hercules and it provides a boost of",Herculesbuff, "to your health, use it well")
-            
-        Defensebuff()
-
+        
     class Hermes():
         def Speedbuff():
             start = 20
@@ -22,9 +19,7 @@ class Blessings():
             randomnum = (random.choice(values))
             Hermesbuff = randomnum
             print ("This charm is imbued with the agility of Hermes and it provides a boost of",Hermesbuff, "to your speed, use it wisely")
-            
-        Speedbuff()
-
+        
     class Artemis():
         def Attackbuff():
             start = 20
@@ -33,8 +28,6 @@ class Blessings():
             randomnum = (random.choice(values))
             Artemisbuff = randomnum
             print ("This charm is imbued with the agility of Hermes and it provides a boost of",Artemisbuff, "to your speed, use it wisely")
-        Attackbuff()
-
 
 Blessings()
 
