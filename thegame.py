4e76bd791862b8937c689e004b9716c53433d8a8
BatuhanCaglar <fbmanbc@gmail.com>
2023-11-22 12:56:00 -0500

    9iythb7hn

--- thegame.py
+++ thegame.py
@@ -3,5 +3,17 @@ import cavalry
 import archers
 import infantry
 
-##
+##Import the blessings
+import demblessings
 
+##Import all the items
+import armor
+
+##Import the enemies
+import enemies 
+
+
+print ("---------")
+print ("Kato Productions Presents")
+print ("BOTS Against the WORLD")
+print ("---------")
