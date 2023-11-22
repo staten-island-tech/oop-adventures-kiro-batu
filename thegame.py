75de2e38589afbc74ec88ead6c86f048c4ac0ca1
BatuhanCaglar <fbmanbc@gmail.com>
2023-11-22 13:03:45 -0500

    aadad

--- thegame.py
+++ thegame.py
@@ -17,3 +17,9 @@ print ("---------")
 print ("Kato Productions Presents")
 print ("BOTS Against the WORLD")
 print ("---------")
+
+
+print ("The world has been run over by the overlord nelahWrM and his monstrous forces")
+print ("It is now your task to bring his tryanny to its knees")
+print ("---")
+print ("As the shady, strange man dressed in violet explain this valiant quest he presents you the figures of ")
