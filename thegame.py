bd0a1750cb708550d71da4c449b43589cfe0a8ec
BatuhanCaglar <fbmanbc@gmail.com>
2023-11-27 13:55:53 -0500

    ghdg

--- thegame.py
+++ thegame.py
@@ -16,10 +16,26 @@ import enemies
 print ("---------")
 print ("Kato Productions Presents")
 print ("BOTS Against the WORLD")
-print ("---------")
+input ("---------")
 
 
-print ("The world has been run over by the overlord nelahWrM and his monstrous forces")
-print ("It is now your task to bring his tryanny to its knees")
-print ("---")
-print ("As the shady, strange man dressed in violet explain this valiant quest he presents you the figures of ")
+def storyline():
+    print ("The world has been run over by the overlord NelahWrM and his monstrous forces")
+    print ("It is now your task to bring his tryanny to its knees")
+    input ("---")
+    print ("As the shady, strange man, dressed in violet, explained this valiant quest he presented you three figures ")
+    input ("---")
+    print ("The first was a posse of lanky men. They possessed long bows half their size")
+    print ("The second was a troup of gruff individuals. They wielded monstrous blades")
+    print ("The last were a formation of majestic horses and skilled riders")
+    input ("---")
+    print ("The figures could come alive and take the form of archers, infantry and cavalry")
+    print ("Each figure possessed their own skill set which was nicely divided into three main stats")
+    print ("These stats were health, attack and speed")
+    input ("---")
+    print ("Yet these stats were random courtesy of the man who carved them")
+storyline()
+
+def intro():
+    from archers import rundown
+    return rundown
