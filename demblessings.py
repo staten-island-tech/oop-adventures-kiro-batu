f6ced369ef703ed096c0f0bac295475ef2b76dd1
BatuhanCaglar <fbmanbc@gmail.com>
2023-12-07 13:34:40 -0500

    as

--- demblessings.py
+++ demblessings.py
@@ -1,16 +1,5 @@
 import random
 
-<<<<<<< HEAD
-class Blessings():
-    class Hercules():
-        def Defensebuff():
-            start = 20
-            end = 25
-            values = list(range(start, end)) 
-            randomnum = (random.choice(values))
-            Herculesbuff = randomnum
-            print ("This charm is imbued with the vitality of Hercules and it provides a boost of",Herculesbuff, "to your health, use it well")
-=======
 class blessing:
     def __init__(self, name, defense, speed, attack):
         self.name = name
@@ -36,13 +25,10 @@ values = list(range(start,end))
 randomnum = (random.choice(values))
 Hermesbuff = randomnum
 ##print ("This charm is imbued with the agility of Hermes and it provides a boost of",Hermesbuff, "to your speed, use it wisely")
->>>>>>> Blessings
+
         
 Hermes = blessing("Hermes", 0, Hermesbuff, 0 )
 
-<<<<<<< HEAD
-
-=======
 ##artemis
 start = 20
 end = 25 
@@ -50,7 +36,6 @@ values = list(range(start, end))
 randomnum = (random.choice(values))
 Artemisbuff = randomnum
 ##print ("This charm is imbued with the agility of Hermes and it provides a boost of",Artemisbuff, "to your speed, use it wisely")
->>>>>>> Blessings
 
 Artemis = blessing("Artemis", 0, 0, Artemisbuff)
 
