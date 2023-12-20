d3ca5772b2c24df496a151f2b9794df7d2d86446
BatuhanCaglar <fbmanbc@gmail.com>
2023-12-20 13:57:34 -0500

    as

--- the_blessings.py
+++ the_blessings.py
@@ -1,4 +1,5 @@
 import random
+from troops import Archer, Infantry, Cavalry
 
 class blessing:
     def __init__(self, name, defense, speed, attack):
@@ -37,4 +38,37 @@ Artemisbuff = randomnum
 ##print ("This charm is imbued with the agility of Hermes and it provides a boost of",Artemisbuff, "to your speed, use it wisely")
 
 Artemis = blessing("Artemis", 0, 0, Artemisbuff)
+
+##attack buff
+def archer_artemis():
+    Archer.attack += Artemis.attack
+    print(Archer.attack)
+def infantry_artemis():
+    Infantry.attack += Artemis.attack
+    print(Infantry.attack)
+def cavalry_artemis():
+    Cavalry.attack += Artemis.attack
+    print(Cavalry.attack)
+
+##speed Buff
+def archer_hermes():
+    Archer.speed += Hermes.speed
+    print(Archer.speed)
+def infantry_hermes():
+    Infantry.speed += Hermes.speed
+    print(Infantry.speed)
+def cavalry_hermes():
+    Cavalry.speed += Hermes.speed
+    print(Cavalry.speed)
+
+##health Buff
+def archer_hercules():
+    Archer.health += Hercules.defense
+    print(Archer.health)
+def infantry_hercules():
+    Infantry.health += Artemis.defense
+    print(Infantry.health)
+def cavalry_hercules():
+    Cavalry.attack += Artemis.defense
+    print(Cavalry.health)
 
